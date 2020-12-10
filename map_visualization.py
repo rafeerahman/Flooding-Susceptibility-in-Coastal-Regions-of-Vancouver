902de8203e0cfbc9c5cc123915e91df4b5fcd576
lorena-b <46202743+lorena-b@users.noreply.github.com>
2020-12-10 00:53:50 -0500

    vis

--- map_visualization.py
+++ map_visualization.py
@@ -1,6 +1,5 @@
 """
 Display the map which shows the susceptible flooding regions - includes interactive features
-GeoPandas seems useful too
 Here we will interpret the data with our own logic/calculations
 MAP DATA FROM
 https://simplemaps.com/data/canada-cities
@@ -12,6 +11,7 @@ import plotly.graph_objects as go  # or plotly.express as px
 
 import pandas as pd
 from typing import Any
+from canada_dsm import *
 
 map = pd.read_csv('vancouver.csv')
 
@@ -19,8 +19,26 @@ map = pd.read_csv('vancouver.csv')
 def display_map(data: Any) -> go.Figure():
     """Displays the map of the given data
     """
+    # Main point
     fig = px.scatter_mapbox(data, lat="lat", lon="lng",
                             color_discrete_sequence=["fuchsia"], zoom=3)
+    # Flood risk points DUMMY VALUES CURRENTLY
+    fig.add_trace(
+        go.Scattermapbox(
+            lat=['49.4', '49.5', '49.6'],
+            lon=['-122.5', '-122.6', '-122.8'],
+            mode='markers',
+            marker=go.scattermapbox.Marker(
+                color='rgb(0, 0, 255)',
+                size=12,
+                colorscale='teal',
+                showscale=True,
+                cmax=200,
+                cmin=0
+            ),
+            text=['Flood Risk'],
+        )
+    )
     fig.update_layout(mapbox_style="open-street-map",
                       mapbox={'center': go.layout.mapbox.Center(lat=data['lat'][0], lon=data['lng'][0]), 'zoom': 10})
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
